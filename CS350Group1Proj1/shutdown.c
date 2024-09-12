56470bc7304753a9289fe3fa86694cd3c57a05cc
Ahmed Aamir <aaamir1@cs.binghamton.edu>
2024-09-12 11:21:43 -0400

    added shutdown command

--- CS350Group1Proj1/shutdown.c
+++ CS350Group1Proj1/shutdown.c
@@ -6,10 +6,10 @@
    stub functions for the system call user space wrapper functions are provided. 
    REMEMBER to disable the stub functions (by commenting the following macro) to 
    allow your implementation to work properly. */
-#define STUB_FUNCS
-#ifdef STUB_FUNCS
-void shutdown(void) {}
-#endif
+// #define STUB_FUNCS
+// #ifdef STUB_FUNCS
+// void shutdown(void) {}
+// #endif
 
 
 /* IMPORTANT INSTRUCTION: the code below should not be changed. 
@@ -21,5 +21,6 @@ main(int argc, char * argv[])
 {
     printf(1, "BYE~\n");
     shutdown();
+    // halt()
     exit(); //return 0;
 }
